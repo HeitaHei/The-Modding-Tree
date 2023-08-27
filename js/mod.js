85ca20ce6c049c5f12e33ef598d5b69419429fd5
HeitaHei <143359613+HeitaHei@users.noreply.github.com>
2023-08-27 23:56:05 +0100

    Update mod.js
    wow stuff

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The World Tree",
+	id: "HeitaTree",
+	author: "Heita",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
